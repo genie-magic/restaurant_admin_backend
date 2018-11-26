e44209d8791cb62119f625fb94a1d61dca981d16
Eric Lee <genie@Erics-iMac.local>
2018-11-26 21:32:12 +0800

    initial commit

---
+++ app/Http/Controllers/API/CityController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\City;
+use App\Http\Resources\City as CityResource;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return CityResource::collection(City::all()->paginate(25));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $city = City::create([
+            'name' => $request->name
+        ]);
+
+        return new CityResource($city);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(City $city)
+    {
+        //
+        return new CityResource($city);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if (isset($request->name)) {
+            City::find($id)->
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
