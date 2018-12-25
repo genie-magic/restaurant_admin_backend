f2e1a989c3580ecea6a79c3e8fc3872be8586d5f
genie-magic <flashingtinker@gmail.com>
2018-12-26 01:19:25 +0800

    add `items` for `menu` resource

--- app/Http/Resources/Menu.php
+++ app/Http/Resources/Menu.php
@@ -5,6 +5,8 @@ namespace App\Http\Resources;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\Restaurant as RestaurantResource;
 use App\Restaurant;
+use App\Http\Resources\Item as ItemResource;
+use App\Item;
 
 class Menu extends JsonResource
 {
@@ -20,6 +22,7 @@ class Menu extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'restaurant' => new RestaurantResource(Restaurant::find($this->restaurant_id)),
+            'items' => $this->items,
             'image_url' => $this->image_url,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at
